bdf85e30e11ccec8175d453c8c5ab3891af47995
popov7 <deniolp@gmail.com>
2019-10-14 23:31:11 +0400

    Configuring webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const {join} = require(`path`);
+
+const path = join(__dirname, `public`);
+
+const entry = `./src/index.js`;
+const output = {
+  filename: `bundle.js`,
+  path,
+};
+
+const contentBase = path;
+const compress = false;
+const hot = true;
+const open = true;
+const port = 1339;
+const devServer = {contentBase, compress, hot, open, port};
+
+const babelizing = {
+  test: /\.(js|jsx)$/,
+  exclude: /node_modules/,
+  use: {
+    loader: `babel-loader`,
+  },
+};
+
+const rules = [babelizing];
+
+module.exports = {
+  entry,
+  output,
+  devServer,
+  module: {rules},
+  devtool: `source-map`,
+};
