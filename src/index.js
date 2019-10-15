20ec42613c3de6d1057087f523dec50d70462704
popov7 <deniolp@gmail.com>
2019-10-15 17:48:17 +0400

    Rendering main page

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from './components/app/app';
+
+const init = () => {
+  ReactDom.render(<App />, document.querySelector(`#root`));
+};
+
+init();
