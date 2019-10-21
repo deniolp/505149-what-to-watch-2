3cfa152d5a3807595ae29430d86ff99b32b7039e
popov7 <deniolp@gmail.com>
2019-10-21 22:43:38 +0400

    Creating mock data for films' cards

---
+++ src/mocks/films.js
@@ -0,0 +1,24 @@
+const filmsMocks = [
+  {
+    name: `Aviator`,
+    src: `img/aviator.jpg`,
+  },
+  {
+    name: `Shutter Island`,
+    src: `img/shutter-island.jpg`,
+  },
+  {
+    name: `Macbeth`,
+    src: `img/macbeth.jpg`,
+  },
+  {
+    name: `Revenant`,
+    src: `img/revenant.jpg`,
+  },
+  {
+    name: `Mindhunter`,
+    src: `img/mindhunter.jpg`,
+  },
+];
+
+export default filmsMocks;
