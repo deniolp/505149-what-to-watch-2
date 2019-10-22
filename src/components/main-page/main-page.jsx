f98ea9fa40ef44d483245a7705468b50fd4f9b0c
popov7 <deniolp@gmail.com>
2019-10-22 19:32:08 +0400

    Creating Footer component

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import FilmsList from '../films-list/films-list';
 import SvgLogo from '../svg-logo/svg-logo';
+import Footer from '../footer/footer';
 
 const MainPage = (props) => {
   const {films} = props;
@@ -152,20 +153,7 @@ const MainPage = (props) => {
           </button>
         </div>
       </section>
-
-      <footer className="page-footer">
-        <div className="logo">
-          <a className="logo__link logo__link--light">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-
-        <div className="copyright">
-          <p>© 2019 What to watch Ltd.</p>
-        </div>
-      </footer>
+      <Footer />
     </div>
   </React.Fragment>;
 };
