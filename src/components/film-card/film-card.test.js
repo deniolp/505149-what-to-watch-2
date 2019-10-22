d73166e4eb627ff85b5e105e265f1345d2f3e3e3
popov7 <deniolp@gmail.com>
2019-10-23 00:31:26 +0400

    Updating tests after adding data to mock

--- src/components/film-card/film-card.test.js
+++ src/components/film-card/film-card.test.js
@@ -9,6 +9,21 @@ describe(`FilmCard`, () => {
     id: 1,
     name: `Aviator`,
     src: `img/aviator.jpg`,
+    genre: `Thriller`,
+    year: 2010,
+    score: 9.4,
+    ratingLevel: `Very good`,
+    ratingCount: 342,
+    description: `Deserunt ex esse tempor aliqua labore ex ipsum ipsum. Minim elit anim consectetur eu cillum. Quis ut ut minim anim sit irure anim non officia id ex. Elit in ea ad elit consequat magna culpa excepteur Lorem. Excepteur sit sit enim irure enim in amet excepteur. Sunt ullamco et cupidatat est adipisicing occaecat nostrud voluptate aliquip consequat commodo adipisicing. Ipsum exercitation est consectetur aute magna anim dolore id enim.
+    Sunt ullamco et cupidatat est adipisicing occaecat nostrud voluptate aliquip consequat commodo adipisicing. Ipsum exercitation est consectetur aute magna anim dolore id enim.`,
+    director: `David Fincher`,
+    starring: [
+      `Daniel Day-Lewis`,
+      `Jack Nicholson`,
+      `Edward Norton`,
+      `Jude Law`,
+      `Willem Dafoe`,
+    ],
   };
 
   it(`renders correctly`, () => {
--- src/components/film-details/film-details.test.js
+++ src/components/film-details/film-details.test.js
@@ -8,6 +8,21 @@ describe(`FilmDetails`, () => {
     id: 1,
     name: `Aviator`,
     src: `img/aviator.jpg`,
+    genre: `Drama`,
+    year: 2008,
+    score: 8.3,
+    ratingLevel: `Good`,
+    ratingCount: 187,
+    description: `Deserunt ex esse tempor aliqua labore ex ipsum ipsum. Minim elit anim consectetur eu cillum. Quis ut ut minim anim sit irure anim non officia id ex. Elit in ea ad elit consequat magna culpa excepteur Lorem. Excepteur sit sit enim irure enim in amet excepteur. Sunt ullamco et cupidatat est adipisicing occaecat nostrud voluptate aliquip consequat commodo adipisicing. Ipsum exercitation est consectetur aute magna anim dolore id enim.
+    Sunt ullamco et cupidatat est adipisicing occaecat nostrud voluptate aliquip consequat commodo adipisicing. Ipsum exercitation est consectetur aute magna anim dolore id enim.`,
+    director: `Christopher Nolan`,
+    starring: [
+      `Bill Murray`,
+      `Jack Nicholson`,
+      `Edward Norton`,
+      `Jude Law`,
+      `Dustin Hoffman`,
+    ],
   };
 
   it(`renders correctly`, () => {
