0e9e8263b3a6d7e4ae63a948a0e11f3ce02807a0
popov7 <deniolp@gmail.com>
2019-10-22 19:06:42 +0400

    Writing simple router

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import MainPage from '../main-page/main-page';
+import FilmDetails from '../film-details/film-details';
 
-const App = ({films}) => {
-  return <MainPage films={films} />;
+const App = (props) => {
+  const getPageScreen = ({films}) => {
+    switch (location.pathname) {
+      case `/`:
+        return <MainPage
+          films={films}
+        />;
+      case `/details`:
+        return <FilmDetails
+          film={films[0]}
+        />;
+    }
+    return null;
+  };
+  return <React.Fragment>{getPageScreen(props)}</React.Fragment>;
 };
 
 App.propTypes = {
   films: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
     src: PropTypes.string.isRequired,
   })),
