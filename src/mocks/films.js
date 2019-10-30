31ce26993e09b57c7020a8a4a1e8083266e03fa7
popov7 <deniolp@gmail.com>
2019-10-30 18:47:37 +0300

    Correcting mocks

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -22,13 +22,13 @@ const filmsMocks = [
     ],
     reviews: [
       {
-        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director&apos;s funniest and most exquisitely designed movies in years.`,
+        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
         author: `Kate Muira`,
         time: `2016-12-24`,
         rating: 8.9,
       },
       {
-        text: `I didn&apos;t find it amusing, and while I can appreciate the creativity, it&apos;s an hour and 40 minutes I wish I could take back.`,
+        text: `I didn't find it amusing, and while I can appreciate the creativity, it's an hour and 40 minutes I wish I could take back.`,
         author: `Amanda Greever`,
         time: `2015-11-18`,
         rating: 8.0,
@@ -46,13 +46,13 @@ const filmsMocks = [
         rating: 7.6,
       },
       {
-        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director&apos;s funniest and most exquisitely designed movies in years.`,
+        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
         author: `Kate Muir`,
         time: `2016-12-24`,
         rating: 8.9,
       },
       {
-        text: `Anderson&apos;s films are too precious for some, but for those of us willing to lose ourselves in them, they&apos;re a delight. &quot;The Grand Budapest Hotel&quot; is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
+        text: `Anderson's films are too precious for some, but for those of us willing to lose ourselves in them, they're a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
         author: `Bill Goodykoontz`,
         time: `2015-11-18`,
         rating: 8.0,
@@ -82,13 +82,13 @@ const filmsMocks = [
     ],
     reviews: [
       {
-        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director&apos;s funniest and most exquisitely designed movies in years.`,
+        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
         author: `Kate Muira`,
         time: `2016-12-24`,
         rating: 8.9,
       },
       {
-        text: `I didn&apos;t find it amusing, and while I can appreciate the creativity, it&apos;s an hour and 40 minutes I wish I could take back.`,
+        text: `I didn't find it amusing, and while I can appreciate the creativity, it's an hour and 40 minutes I wish I could take back.`,
         author: `Amanda Greever`,
         time: `2015-11-18`,
         rating: 8.0,
@@ -106,13 +106,13 @@ const filmsMocks = [
         rating: 7.6,
       },
       {
-        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director&apos;s funniest and most exquisitely designed movies in years.`,
+        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
         author: `Kate Muir`,
         time: `2016-12-24`,
         rating: 8.9,
       },
       {
-        text: `Anderson&apos;s films are too precious for some, but for those of us willing to lose ourselves in them, they&apos;re a delight. &quot;The Grand Budapest Hotel&quot; is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
+        text: `Anderson's films are too precious for some, but for those of us willing to lose ourselves in them, they're a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
         author: `Bill Goodykoontz`,
         time: `2015-11-18`,
         rating: 8.0,
@@ -142,13 +142,13 @@ const filmsMocks = [
     ],
     reviews: [
       {
-        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director&apos;s funniest and most exquisitely designed movies in years.`,
+        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
         author: `Kate Muira`,
         time: `2016-12-24`,
         rating: 8.9,
       },
       {
-        text: `I didn&apos;t find it amusing, and while I can appreciate the creativity, it&apos;s an hour and 40 minutes I wish I could take back.`,
+        text: `I didn't find it amusing, and while I can appreciate the creativity, it's an hour and 40 minutes I wish I could take back.`,
         author: `Amanda Greever`,
         time: `2015-11-18`,
         rating: 8.0,
@@ -166,13 +166,13 @@ const filmsMocks = [
         rating: 7.6,
       },
       {
-        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director&apos;s funniest and most exquisitely designed movies in years.`,
+        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
         author: `Kate Muir`,
         time: `2016-12-24`,
         rating: 8.9,
       },
       {
-        text: `Anderson&apos;s films are too precious for some, but for those of us willing to lose ourselves in them, they&apos;re a delight. &quot;The Grand Budapest Hotel&quot; is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
+        text: `Anderson's films are too precious for some, but for those of us willing to lose ourselves in them, they're a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
         author: `Bill Goodykoontz`,
         time: `2015-11-18`,
         rating: 8.0,
@@ -202,13 +202,13 @@ const filmsMocks = [
     ],
     reviews: [
       {
-        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director&apos;s funniest and most exquisitely designed movies in years.`,
+        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
         author: `Kate Muira`,
         time: `2016-12-24`,
         rating: 8.9,
       },
       {
-        text: `I didn&apos;t find it amusing, and while I can appreciate the creativity, it&apos;s an hour and 40 minutes I wish I could take back.`,
+        text: `I didn't find it amusing, and while I can appreciate the creativity, it's an hour and 40 minutes I wish I could take back.`,
         author: `Amanda Greever`,
         time: `2015-11-18`,
         rating: 8.0,
@@ -226,13 +226,13 @@ const filmsMocks = [
         rating: 7.6,
       },
       {
-        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director&apos;s funniest and most exquisitely designed movies in years.`,
+        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
         author: `Kate Muir`,
         time: `2016-12-24`,
         rating: 8.9,
       },
       {
-        text: `Anderson&apos;s films are too precious for some, but for those of us willing to lose ourselves in them, they&apos;re a delight. &quot;The Grand Budapest Hotel&quot; is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
+        text: `Anderson's films are too precious for some, but for those of us willing to lose ourselves in them, they're a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
         author: `Bill Goodykoontz`,
         time: `2015-11-18`,
         rating: 8.0,
@@ -262,13 +262,13 @@ const filmsMocks = [
     ],
     reviews: [
       {
-        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director&apos;s funniest and most exquisitely designed movies in years.`,
+        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
         author: `Kate Muira`,
         time: `2016-12-24`,
         rating: 8.9,
       },
       {
-        text: `I didn&apos;t find it amusing, and while I can appreciate the creativity, it&apos;s an hour and 40 minutes I wish I could take back.`,
+        text: `I didn't find it amusing, and while I can appreciate the creativity, it's an hour and 40 minutes I wish I could take back.`,
         author: `Amanda Greever`,
         time: `2015-11-18`,
         rating: 8.0,
@@ -286,13 +286,13 @@ const filmsMocks = [
         rating: 7.6,
       },
       {
-        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director&apos;s funniest and most exquisitely designed movies in years.`,
+        text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
         author: `Kate Muir`,
         time: `2016-12-24`,
         rating: 8.9,
       },
       {
-        text: `Anderson&apos;s films are too precious for some, but for those of us willing to lose ourselves in them, they&apos;re a delight. &quot;The Grand Budapest Hotel&quot; is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
+        text: `Anderson's films are too precious for some, but for those of us willing to lose ourselves in them, they're a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
         author: `Bill Goodykoontz`,
         time: `2015-11-18`,
         rating: 8.0,
