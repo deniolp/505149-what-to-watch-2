7efce3591c001b47993b3a69d50955cf73893609
popov7 <deniolp@gmail.com>
2019-11-13 18:22:49 +0300

    Creating reducer

--- src/reducer.js
+++ src/reducer.js
@@ -20,4 +20,18 @@ const ActionCreator = {
   }
 };
 
-export {ActionCreator};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case `CHANGE_GENRE`: return Object.assign({}, state, {
+      genre: action.payload,
+    });
+
+    case `GET_FILTERED_FILMS`: return Object.assign({}, state, {
+      films: action.payload,
+    });
+  }
+
+  return state;
+};
+
+export {ActionCreator, reducer};
