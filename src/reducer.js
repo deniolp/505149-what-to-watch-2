a1c137be438ad2dcbbdb047321ca04e2ce0f9e94
popov7 <deniolp@gmail.com>
2019-11-13 18:20:07 +0300

    Creating two actions

--- src/reducer.js
+++ src/reducer.js
@@ -4,3 +4,20 @@ const initialState = {
   genre: `allGenres`,
   films: filmsMocks,
 };
+
+const ActionCreator = {
+  changeGenre: (selectedGenre) => ({
+    type: `CHANGE_GENRE`,
+    payload: selectedGenre,
+  }),
+  getFilms: (selectedGenre, films) => {
+    const filteredFilms = films.filter((film) => film.genre === selectedGenre);
+
+    return {
+      type: `GET_FILTERED_FILMS`,
+      payload: filteredFilms,
+    };
+  }
+};
+
+export {ActionCreator};
