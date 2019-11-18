cf33d433a2ff0432f18367c2649a47d616754af2
popov7 <deniolp@gmail.com>
2019-11-18 13:56:41 +0300

    Using thunk and api in index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {compose} from 'recompose';
 
 import App from './components/app/app';
-import {reducer} from './reducer/reducer';
+import {reducer, Operation} from './reducer/reducer';
+import createAPI from './api';
 
 const init = () => {
-  const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
-  );
+  const api = createAPI((...args) => store.dispatch(...args));
+
+  const store = createStore(
+      reducer,
+      compose(
+          applyMiddleware(thunk.withExtraArgument(api)),
+          window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+      ));
+
+  store.dispatch(Operation.loadFilms());
 
   ReactDom.render(
       <Provider store={store}>
