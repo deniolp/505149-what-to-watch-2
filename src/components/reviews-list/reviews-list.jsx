5c0e0967be85992b270df416de90a25f0915e170
popov7 <deniolp@gmail.com>
2019-11-18 15:49:12 +0300

    Correcting of showing reviews implementation

--- src/components/reviews-list/reviews-list.jsx
+++ src/components/reviews-list/reviews-list.jsx
@@ -5,16 +5,20 @@ import Review from '../review/review';
 
 const ReviewsList = (props) => {
   const {reviews} = props;
-  const middle = Math.floor(reviews.length / 2);
-  const reviewsFirstPart = reviews.slice(0, middle);
-  const reviewsSecondPart = reviews.slice(middle, reviews.length);
+  let reviewsFirstPart;
+  let reviewsSecondPart;
+  if (reviews) {
+    const middle = Math.floor(reviews.length / 2);
+    reviewsFirstPart = reviews.slice(0, middle);
+    reviewsSecondPart = reviews.slice(middle, reviews.length);
+  }
 
   return <div className="movie-card__reviews movie-card__row">
     <div className="movie-card__reviews-col">
-      {reviewsFirstPart.map((review) => <Review review={review} key={`${review.author}${review.time}`}></Review>)}
+      {reviewsFirstPart && reviewsFirstPart.map((review) => <Review review={review} key={`${review.author}${review.time}`}></Review>)}
     </div>
     <div className="movie-card__reviews-col">
-      {reviewsSecondPart.map((review) => <Review review={review} key={`${review.author}${review.time}`}></Review>)}
+      {reviewsSecondPart && reviewsSecondPart.map((review) => <Review review={review} key={`${review.author}${review.time}`}></Review>)}
     </div>
   </div>;
 };
