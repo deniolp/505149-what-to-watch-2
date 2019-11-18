b8eaea16323f5d9e6ac76c42deac430111e89be6
popov7 <deniolp@gmail.com>
2019-11-18 13:54:47 +0300

    Creating api.js with createAPI func

---
+++ src/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+import {ActionCreator} from './reducer/reducer';
+
+const createAPI = (dispatch) => {
+  const api = axios.create({
+    baseURL: `https://htmlacademy-react-2.appspot.com/wtw`,
+    timeout: 5000,
+    withCredentials: true,
+  });
+
+  const onSuccess = (response) => response;
+
+  const onFail = (error) => {
+    if (error.response === 403) {
+      dispatch(ActionCreator.requireAuthorization(true));
+    }
+    return error;
+  };
+  api.interceptors.response.use(onSuccess, onFail);
+
+  return api;
+};
+
+export default createAPI;
