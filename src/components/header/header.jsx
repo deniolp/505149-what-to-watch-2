3b9392712ea60c4af7e05e3852e02293e077c1df
popov7 <deniolp@gmail.com>
2019-11-19 14:38:42 +0300

    Showing avatar of user gotted from server

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
-const Header = () => {
+const Header = (props) => {
+  const {user} = props;
+
   const path = location.pathname === `/` ? null : `/`;
+
   return <React.Fragment>
     <h1 className="visually-hidden">WTW</h1>
     <header className="page-header movie-card__head">
@@ -15,11 +21,34 @@ const Header = () => {
 
       <div className="user-block">
         <div className="user-block__avatar">
-          <img src="/img/avatar.jpg" alt="User avatar" width="63" height="63" />
+          {!user.avatarUrl ? <p>
+            <Link
+              to="/favorites"
+              style={{
+                color: `#dfcf77`,
+                textDecoration: `none`,
+              }}
+            >Sign in
+            </Link>
+          </p> : <img src={`https://htmlacademy-react-2.appspot.com${user.avatarUrl}`} alt="User avatar" width="63" height="63" />}
         </div>
       </div>
     </header>
   </React.Fragment>;
 };
 
-export default Header;
+Header.propTypes = {
+  user: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    email: PropTypes.string,
+    avatarUrl: PropTypes.string,
+  }),
+};
+
+const mapStateToProps = (state, ownProps) => Object.assign({}, ownProps, {
+  user: state.user,
+});
+
+export {Header};
+export default connect(mapStateToProps, null)(Header);
