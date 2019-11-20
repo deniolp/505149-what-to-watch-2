547de06820e8a7c8d688232a60cf007a9c8346f2
popov7 <deniolp@gmail.com>
2019-11-20 17:03:40 +0300

    Updating tests

--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {BrowserRouter} from 'react-router-dom';
 
 import {Header} from './header';
 
 describe(`Header`, () => {
   it(`renders correctly`, () => {
     const tree = renderer.create(
-        <Header
+        <BrowserRouter><Header
           user={{
             id: 1,
             name: `Olga`,
             email: `r@ya.ru`,
             avatarUrl: `/img/photo.jpg`,
           }}
-        />).toJSON();
+        /></BrowserRouter>).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
