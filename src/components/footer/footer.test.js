df607173c69f62fb0530782fa638e21ec7aa4c37
popov7 <deniolp@gmail.com>
2019-11-23 17:59:55 +0300

    Updating tests

--- src/components/footer/footer.test.js
+++ src/components/footer/footer.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {BrowserRouter} from 'react-router-dom';
 
 import Footer from './footer';
 
 describe(`Footer`, () => {
   it(`renders correctly`, () => {
     const tree = renderer.create(
-        <Footer/>).toJSON();
+        <BrowserRouter>
+          <Footer/>
+        </BrowserRouter>).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
