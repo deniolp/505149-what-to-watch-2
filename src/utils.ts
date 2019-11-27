c31a04ab9e26e2524bb7a45eaff3820055ec6f96
popov7 <deniolp@gmail.com>
2019-11-27 16:12:38 +0300

    Adding types to returns of funcs

---
+++ src/utils.ts
@@ -0,0 +1,29 @@
+import {Film} from './types';
+
+const snakeToCamel = (word: string): string => word.replace(/(_\w)/g, (matches) => matches[1].toUpperCase());
+
+const normalizeKeys = (obj: object): object => {
+  if (Array.isArray(obj)) {
+    return obj.map((item) => normalizeKeys(item));
+  }
+
+  if (obj !== null && obj.constructor === Object) {
+    return Object.keys(obj).reduce((acc, key) => Object.assign(acc, {
+      [snakeToCamel(key)]: normalizeKeys(obj[key]),
+    }), {});
+  }
+
+  return obj;
+};
+
+const changeFilm = (films: Film[], newFilm: Film): Film[] => {
+  return films.map((item) => {
+    return item.id === newFilm.id ? newFilm : item;
+  });
+};
+
+const changeVideoUrl = (films: Film[]) => films.map((item) => {
+  item.previewVideoLink = `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`;
+});
+
+export {normalizeKeys, changeFilm, changeVideoUrl};
